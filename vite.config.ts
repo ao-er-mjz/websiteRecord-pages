9fb0b063880eb44b76a2d5d9b5850023e6b7a9d7
mai <13316035435@ydt24.com>
2025-02-11 14:14:33 +0800

    打包配置修改；

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import UnoCSS from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/websiteRecord-pages/',
+    base: './',
     plugins: [UnoCSS(), react()],
     server: {
         proxy: {
