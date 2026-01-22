3bfec7dc9d4411ec91745df50c81680e17a5043e
mai <13316035435@ydt24.com>
2026-01-22 10:43:10 +0800

    feat: 添加静态站点托管配置文件

---
+++ src/const/静态站点托管.ts
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'GitHub Pages',
+        text: '静态站点托管，Git集成，无限流量，适合个人博客/开源文档',
+        img: 'githubPagesImg',
+        href: 'https://pages.github.com/',
+    },
+    {
+        name: 'Gitee Pages',
+        text: '静态站点托管，Git集成，国内访问快，自定义域名需备案',
+        img: 'giteePagesImg',
+        href: 'https://gitee.com/help/articles/4136',
+    },
+    {
+        name: 'Netlify',
+        text: '静态站点托管，Git集成，自动部署，支持Serverless函数',
+        img: 'netlifyImg',
+        href: 'https://www.netlify.com/',
+    },
+    {
+        name: 'Vercel',
+        text: '静态站点托管，Git集成，Next.js优化，100GB/月免费流量',
+        img: 'vercelImg',
+        href: 'https://vercel.com/',
+    },
+    {
+        name: 'GitLab Pages',
+        text: '静态站点托管，Git集成，CI/CD流水线，支持私有仓库',
+        img: 'gitlabPagesImg',
+        href: 'https://about.gitlab.com/stages-devops-lifecycle/pages/',
+    },
+];
